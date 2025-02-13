8e3f1db135829aaba15b882b9c6b6dbab8766bb2
ProjectYaroslav <arturyarik2010@gmail.com>
2025-02-14 01:13:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,3 @@ document.getElementById('startBtn').onclick = function() {
     window.location.href = 'heroes.html';
 };
 
-document.getElementById('linkButton1').onclick = function() {
-    window.location.href = 'https://librebook.me/abai_s_way/vol1/1';
-};
-
-document.getElementById('linkButton2').onclick = function() {
-    window.location.href = 'https://librebook.me/altyn_tolobas';
-};
-
-document.getElementById('linkButton3').onclick = function() {
-    window.location.href = 'https://libcat.ru/knigi/proza/istoricheskaya-proza/314049-bulat-zhandarbekov-tomiris.html#text';
-};
-
-document.getElementById('linkButton4').onclick = function() {
-    window.location.href = 'https://libcat.ru/knigi/proza/istoricheskaya-proza/314049-bulat-zhandarbekov-tomiris.html#text';
-};
-
